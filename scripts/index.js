5ce75b3f92cda8786977b2b18f7a585d0aa2d571
josueliasl <josue.elias.leyva@gmail.com>
2025-05-07 22:43:54 -0600

    responsive css

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,15 @@
 let currentYear = new Date().getFullYear();
 let lastModified = document.lastModified;
 
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () =>{
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show')
+})
+
+
 document.getElementById("currentYear").textContent = `©${currentYear}`;
 document.getElementById("lastModified").textContent = ` Last Modification: ${lastModified}`;
 
