4fa10203a5fbfcecd0c7d9466ca2f4a51a8d2de9
josueliasl <josue.elias.leyva@gmail.com>
2025-05-14 18:41:04 -0600

    js

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,5 @@
+let currentYear = new Date().getFullYear();
+let lastModified = document.lastModified;
+
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
