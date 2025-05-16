264bce5fed579ee7eacc1e44477e9748f1a5a1c5
josueliasl <josue.elias.leyva@gmail.com>
2025-05-15 22:15:25 -0600

    js raw directory

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -14,10 +14,57 @@ document.getElementById("lastModified").textContent = ` Last Modification: ${las
 function toggleActive(element) {
     element.classList.toggle('active');
 }
+let businesses = [];
+const url = 'data/members.json';
 
+async function getBusinessData() {
+    const response = await fetch(url);
+    const data = await response.json();
+    businesses = data.business; // temporary testing of data response
+}
 
-const business = [
-    {
-        
-    }
-]
+getBusinessData();
+
+const container = document.getElementById('container');
+const bgrid = document.getElementById('bgrid');
+const blist = document.getElementById('blist');
+
+function renderGrid() {
+    container.className = 'grid';
+    container.innerHTML = '';
+    businesses.forEach(business => {
+        const card = document.createElement('div');
+        card.className = 'card';
+
+        const icon = document.createElement('img');
+        icon.src = `images/${business.icon}`;
+        icon.alt = business.name;
+
+        const name = document.createElement('h3');
+        name.textContent = business.name;
+
+        const description = document.createElement('p');
+        description.textContent = business.description;
+
+        card.appendChild(icon);
+        card.appendChild(name);
+        card.appendChild(description);
+        container.appendChild(card);
+    });
+}
+
+function renderList() {
+    container.className = 'list';
+    container.innerHTML = '';
+    const ul = document.createElement('ul');
+    businesses.forEach(business => {
+        const li = document.createElement('li');
+        li.innerHTML = `${business.name}<br>${business.address}<br>${business.phone}<br>${business.website}<br>${business.description}`;
+        ul.appendChild(li);
+    });
+    container.appendChild(ul);
+}
+renderGrid();
+
+bgrid.addEventListener('click', renderGrid);
+blist.addEventListener('click', renderList);
