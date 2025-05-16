fb72fe0d9d78b301252545582634d39b11c7891b
josueliasl <josue.elias.leyva@gmail.com>
2025-05-15 19:47:59 -0600

    hambutto

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -1,5 +1,16 @@
 let currentYear = new Date().getFullYear();
 let lastModified = document.lastModified;
 
-document.getElementById("currentYear").textContent = currentYear;
-document.getElementById("lastModified").textContent = `Last Modification: ${lastModified}`;
+const mainnav = document.querySelector('.navigation')
+const hambutton = document.querySelector('#menu');
+
+hambutton.addEventListener('click', () => {
+    mainnav.classList.toggle('show');
+    hambutton.classList.toggle('show')
+})
+document.getElementById("currentYear").textContent = `©${currentYear}`;
+document.getElementById("lastModified").textContent = ` Last Modification: ${lastModified}`;
+
+function toggleActive(element) {
+    element.classList.toggle('active');
+}
