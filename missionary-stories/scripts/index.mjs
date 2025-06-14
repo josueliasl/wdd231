3b6d86ce2855bd05ddf975c2d99e491d70b3338e
josueliasl <josue.elias.leyva@gmail.com>
2025-06-14 13:23:56 -0600

    over?

--- missionary-stories/scripts/index.mjs
+++ missionary-stories/scripts/index.mjs
@@ -2,7 +2,16 @@
 import { getStoryData, renderStory, displayStoryDetails } from './stories.js';
 import { menu } from './nav.js';
 import { apiFetch } from './weather.js';
+import { displaytimestamp, formAnswers } from './share.js';
+
 document.addEventListener("DOMContentLoaded", () => {
+    const path = window.location.pathname;
+    if (path.includes("form.html")) {
+        displaytimestamp();
+    }
+    if (path.includes("thankyou.html")) {
+        formAnswers();
+    }
     const container = document.getElementById('storiesContainer');
     const storyDetails = document.getElementById("story-details");
 
--- missionary-stories/scripts/share.js
+++ missionary-stories/scripts/share.js
@@ -1,7 +1,22 @@
-const myInfo = new URLSearchParams(window.location.search);
+export function displaytimestamp() {
+    const form = document.querySelector('form');
+    const timestampInput = document.getElementById("timestamp")
+    if (form && timestampInput){ 
+    timestampInput.value = new Date().toISOString();
 
-document.querySelector('#results').innerHTML = `
+    form.addEventListener('submit', function () {
+        timestampInput.value = new Date().toISOString();
+    });
+}
+}
+export function formAnswers(){
+const myInfo = new URLSearchParams(window.location.search);
+const results = document.querySelector('#results');
+
+if(results){ 
+results.innerHTML = `
 <p>Your name is: ${myInfo.get('first')} ${myInfo.get('last')}</p>
 <p>Your mission was: ${myInfo.get('mission')} </p>
 <p>Story: ${myInfo.get('story')} </p>
 <p>${myInfo.get('timestamp')}</p>`;
+}}
